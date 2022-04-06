f67e1391c418ca97f6fe9e80c663f95c762ebdda
Dan Vanderkam <danvdk@gmail.com>
2022-04-06 19:27:34 -0400

    Fill in some gaps in test coverage (#16)
    * Tests for update all
    
    * Update with fixed columns and a Set parameter
    
    * test for update w/ fixed columns and limitOne
    
    * Unit tests for dynamic update with a where any clause
    
    * e2e test for update with a where any clause

--- test/e2e/update-e2e.test.ts
+++ test/e2e/update-e2e.test.ts
@@ -85,6 +85,44 @@ describe('update e2e', () => {
     `);
   });
 
+  it('should update with a where any clause', async () => {
+    const update = docTable.update({
+      where: [any('title')],
+    });
+    expect(
+      await update(
+        db,
+        {title: ['Vision 2023', 'Annual Plan for 2022']},
+        {contents: 'Looking gloomy'},
+      ),
+    ).toMatchInlineSnapshot(`
+      Array [
+        Object {
+          "contents": "Looking gloomy",
+          "created_by": "dee5e220-1f62-4f80-ad29-3ad48a03a36e",
+          "id": "cde34b31-1f62-4f80-ad29-3ad48a03a36e",
+          "title": "Annual Plan for 2022",
+        },
+        Object {
+          "contents": "Looking gloomy",
+          "created_by": "d0e23a20-1f62-4f80-ad29-3ad48a03a47f",
+          "id": "01234b31-1f62-4f80-ad29-3ad48a03a36e",
+          "title": "Vision 2023",
+        },
+      ]
+    `);
+    expect(await docTable.select()(db)).toMatchObject([
+      {
+        title: 'Annual Plan for 2022',
+        contents: 'Looking gloomy',
+      },
+      {
+        title: 'Vision 2023',
+        contents: 'Looking gloomy',
+      },
+    ]);
+  });
+
   it('should update with fixed columns', async () => {
     const update = docTable.update({set: ['contents'], where: ['title']});
     expect(
@@ -114,6 +152,50 @@ describe('update e2e', () => {
     `);
   });
 
+  it('should update with fixed columns and limitOne', async () => {
+    const update = docTable.update({
+      set: ['contents'],
+      where: ['title'],
+      limitOne: true,
+    });
+    expect(
+      await update(
+        db,
+        {title: 'Annual Plan for 2022'},
+        {contents: 'Twas the best of times, err, I mean…'},
+      ),
+    ).toMatchInlineSnapshot(`
+      Object {
+        "contents": "Twas the best of times, err, I mean…",
+        "created_by": "dee5e220-1f62-4f80-ad29-3ad48a03a36e",
+        "id": "cde34b31-1f62-4f80-ad29-3ad48a03a36e",
+        "title": "Annual Plan for 2022",
+      }
+    `);
+    expect(await getDocByTitle(db, {title: 'Annual Plan for 2022'}))
+      .toMatchInlineSnapshot(`
+      Array [
+        Object {
+          "contents": "Twas the best of times, err, I mean…",
+          "title": "Annual Plan for 2022",
+        },
+      ]
+    `);
+  });
+
+  it('should update without a where clause (update all)', async () => {
+    const getAllDocs = docTable.select();
+    const update = docTable.update({set: ['contents']});
+    expect(await update(db, {}, {contents: 'This and that'})).toMatchObject([
+      {title: 'Annual Plan for 2022', contents: 'This and that'},
+      {title: 'Vision 2023', contents: 'This and that'},
+    ]);
+    expect(await getAllDocs(db)).toMatchObject([
+      {title: 'Annual Plan for 2022', contents: 'This and that'},
+      {title: 'Vision 2023', contents: 'This and that'},
+    ]);
+  });
+
   it('should update with an any clause', async () => {
     const getAllDocs = docTable.select();
     const update = docTable.update({
@@ -124,6 +206,29 @@ describe('update e2e', () => {
       await update(
         db,
         {title: ['Vision 2023', 'Annual Plan for 2022']},
+        {contents: 'To Be Written'},
+      ),
+    ).toMatchObject([
+      {title: 'Annual Plan for 2022', contents: 'To Be Written'},
+      {title: 'Vision 2023', contents: 'To Be Written'},
+    ]);
+
+    expect(await getAllDocs(db)).toMatchObject([
+      {title: 'Annual Plan for 2022', contents: 'To Be Written'},
+      {title: 'Vision 2023', contents: 'To Be Written'},
+    ]);
+  });
+
+  it('should update with an any clause and a Set parameter', async () => {
+    const getAllDocs = docTable.select();
+    const update = docTable.update({
+      set: ['contents'],
+      where: [any('title')],
+    });
+    expect(
+      await update(
+        db,
+        {title: new Set(['Vision 2023', 'Annual Plan for 2022'])},
         {contents: 'To Be Written'},
       ),
     ).toMatchObject([
--- test/unit/update-unit.test.ts
+++ test/unit/update-unit.test.ts
@@ -47,6 +47,38 @@ describe('update', () => {
     `);
   });
 
+  it('should update with a where any clause', async () => {
+    const update = docTable.update({where: [any('title')]});
+    await update(
+      mockDb,
+      {title: ['Great Expectations']},
+      {created_by: 'Charles Dickens'},
+    );
+
+    expect(mockDb.q).toMatchInlineSnapshot(
+      `"UPDATE doc SET created_by = $2 WHERE title = ANY($1) RETURNING *"`,
+    );
+    expect(mockDb.args).toMatchInlineSnapshot(`
+      Array [
+        Array [
+          "Great Expectations",
+        ],
+        "Charles Dickens",
+      ]
+    `);
+    const arrayQ = mockDb.q;
+    const arrayArgs = mockDb.args;
+
+    // Passing a Set should be the same as an Array.
+    await update(
+      mockDb,
+      {title: new Set(['Great Expectations'])},
+      {created_by: 'Charles Dickens'},
+    );
+    expect(mockDb.q).toEqual(arrayQ);
+    expect(mockDb.args).toEqual(arrayArgs);
+  });
+
   it('should update with fixed columns', async () => {
     const update = docTable.update({set: ['contents'], where: ['title']});
     await update(
@@ -66,6 +98,43 @@ describe('update', () => {
     `);
   });
 
+  it('should update with fixed columns and limitOne', async () => {
+    const update = docTable.update({
+      set: ['contents'],
+      where: ['title'],
+      limitOne: true,
+    });
+    await update(
+      mockDb,
+      {title: 'Great Expectations'},
+      {contents: 'Twas the best of times, err, I mean…'},
+    );
+
+    expect(mockDb.q).toMatchInlineSnapshot(
+      `"UPDATE doc SET contents = $1 WHERE title = $2 RETURNING *"`,
+    );
+    expect(mockDb.args).toMatchInlineSnapshot(`
+      Array [
+        "Twas the best of times, err, I mean…",
+        "Great Expectations",
+      ]
+    `);
+  });
+
+  it('should update without a where clause (update all)', async () => {
+    const update = docTable.update({set: ['created_by']});
+    await update(mockDb, {}, {created_by: 'A Person'});
+
+    expect(mockDb.q).toMatchInlineSnapshot(
+      `"UPDATE doc SET created_by = $1 RETURNING *"`,
+    );
+    expect(mockDb.args).toMatchInlineSnapshot(`
+      Array [
+        "A Person",
+      ]
+    `);
+  });
+
   it('should update with an any clause', async () => {
     const update = docTable.update({
       set: ['created_by'],
@@ -74,6 +143,31 @@ describe('update', () => {
     await update(
       mockDb,
       {title: ['Great Expectations', 'Bleak House']},
+      {created_by: 'Charles Dickens'},
+    );
+
+    expect(mockDb.q).toMatchInlineSnapshot(
+      `"UPDATE doc SET created_by = $1 WHERE title = ANY($2) RETURNING *"`,
+    );
+    expect(mockDb.args).toMatchInlineSnapshot(`
+      Array [
+        "Charles Dickens",
+        Array [
+          "Great Expectations",
+          "Bleak House",
+        ],
+      ]
+    `);
+  });
+
+  it('should update with an any clause and a Set parameter', async () => {
+    const update = docTable.update({
+      set: ['created_by'],
+      where: [any('title')],
+    });
+    await update(
+      mockDb,
+      {title: new Set(['Great Expectations', 'Bleak House'])},
       {created_by: 'Charles Dickens'},
     );
 
