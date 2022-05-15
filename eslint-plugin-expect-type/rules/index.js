aa731aa0746888832b7dab4c8d150c4236a74024
Dan Vanderkam <danvdk@gmail.com>
2022-05-14 20:46:43 -0400

    Un-vendor eslint-plugin-expect-type (#21)

--- eslint-plugin-expect-type/rules/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.rules = void 0;
-const expect_1 = require("./expect");
-exports.rules = {
-    [expect_1.name]: expect_1.rule,
-};
